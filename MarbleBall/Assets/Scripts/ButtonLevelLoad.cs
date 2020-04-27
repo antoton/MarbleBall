92ef7d959ff34f6f8fb1b3855b0168aa67e6f5cc
Jens Uenk <jens.uenk@gmail.com>
2020-04-27 12:40:37 +0200

    Created start platform + ball

---
+++ MarbleBall/Assets/Scripts/ButtonLevelLoad.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonLevelLoad : MonoBehaviour
+{
+    public string mLevelToLoad;
+    // Start is called before the first frame update
+    public void LoadLevel() {
+        // Load the new Level (mLevelToLoad)
+        SceneManager.LoadScene(mLevelToLoad);
+    }
+}
